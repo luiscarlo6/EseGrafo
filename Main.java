d576d9c9c31197726642e961ed5fa8c1fb528bb1
luiscarlo6 <luiscarlo6@gmail.com>
2012-09-29 16:22:20 -0430

    AAAA

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ public class Main {
     	a1 = new Arco("dos","tres");
     	a2 = new Arco("tres","cuatro");
     	
-   // 	System.out.println("\nEstos son Arcos=\n");
-    //	System.out.println(a1.toString());
-    	//System.out.println(a2.toString());
+	System.out.println("\nEstos son Arcos=\n");
+	System.out.println(a1.toString());
+    	System.out.println(a2.toString());
     	
 
     	
