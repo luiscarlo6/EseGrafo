66272a3bdb40b092fd8b920419d0efaab918fcb1
luiscarlo6 <luiscarlo6@gmail.com>
2012-09-29 16:25:55 -0430

    PROBANDO

--- Main.java
+++ Main.java
@@ -11,12 +11,10 @@ public class Main {
     	a1 = new Arco("dos","tres");
     	a2 = new Arco("tres","cuatro");
     	
-	System.out.println("\nEstos son Arcos=\n");
-	System.out.println(a1.toString());
-    	System.out.println(a2.toString());
-    	
-
-    	
+	//System.out.println("\nEstos son Arcos=\n");
+	//System.out.println(a1.toString());
+    	//System.out.println(a2.toString());
+    	   	
     	iguales = a1.equals(a2);
     	
     	System.out.println(iguales);
